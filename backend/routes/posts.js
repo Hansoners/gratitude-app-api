3ecafadff4ffb623b9ad6e4d9e8ee58fbcf9078e
hglchen <hglchen@uwaterloo.ca>
2020-08-29 01:54:55 -0400

    Routes fix.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,4 +1,5 @@
 const express = require("express");
+
 const Post = require("../models/post");
 
 const router = express.Router();
@@ -16,7 +17,7 @@ router.post("", (req, res, next) => {
     });
 });
 
-router.put(":id", (req, res, next) => {
+router.put("/:id", (req, res, next) => {
     const post = new Post({
         _id: req.body.id,
         title: req.body.title,
@@ -36,7 +37,7 @@ router.get("", (req, res, next) => {
     });
 });
 
-router.get(":id", (req, res, next) => {
+router.get("/:id", (req, res, next) => {
     Post.findById(req.params.id).then(post => {
         if (post) {
             res.status(200).json(post);
@@ -46,7 +47,7 @@ router.get(":id", (req, res, next) => {
     });
 })
 
-router.delete(":id", (req, res, next) => {
+router.delete("/:id", (req, res, next) => {
     Post.deleteOne({ _id: req.params.id }).then(result => {
         console.log(result);
         res.status(200).json({ message: "Post deleted!" });
