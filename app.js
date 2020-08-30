874a61e97730d905e3931afdf2e7d10948445475
hglchen <hglchen@uwaterloo.ca>
2020-08-30 19:21:33 -0400

    refactor files for heroku deploy.

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const postRoutes = require("./routes/posts");
-const userRoutes = require("./routes/users");
+const postRoutes = require("./backend/routes/posts");
+const userRoutes = require("./backend/routes/users");
 
 const app = express();
 
