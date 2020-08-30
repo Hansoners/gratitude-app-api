a8b2f9b9db3f1ede49d8028bc0747dbc19d65cb8
hglchen <hglchen@uwaterloo.ca>
2020-08-30 17:57:08 -0400

    Refactored for aws deployment.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const app = require("./backend/app");
+const app = require("./app");
 const debug = require("debug")("node-angular");
 const http = require("http");
 
@@ -6,12 +6,10 @@ const normalizePort = val => {
   var port = parseInt(val, 10);
 
   if (isNaN(port)) {
-    // named pipe
     return val;
   }
 
   if (port >= 0) {
-    // port number
     return port;
   }
 
