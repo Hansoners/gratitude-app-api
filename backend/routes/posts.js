bae5dab26443c2e62fd2912c14261601e67e18f0
hglchen <hglchen@uwaterloo.ca>
2020-08-29 23:28:40 -0400

    Refactor js files.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,65 +1,19 @@
 const express = require("express");
 
-const Post = require("../models/post");
+const postController = require("../controllers/posts")
+
 const checkAuth = require("../middleware/check-auth");
+
 const router = express.Router();
 
-router.post("", checkAuth, (req, res, next) => {
-    const post = new Post({
-        title: req.body.title,
-        content: req.body.content,
-        creator: req.userData.userId
-    });
-    post.save().then(createdPost => {
-        res.status(201).json({
-            message: "Post added successfully!",
-            postId: createdPost._id
-        });
-    });
-});
+router.post("", checkAuth, postController.createPost);
 
-router.put("/:id", checkAuth, (req, res, next) => {
-    const post = new Post({
-        _id: req.body.id,
-        title: req.body.title,
-        content: req.body.content
-    });
-    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-        if (result.n > 0) {
-            res.status(200).json({ message: "Update successful!" });
-        } else {
-            res.status(401).json({ message: "Unauthorized PUT request." });
-        }
-    });
-});
+router.put("/:id", checkAuth, postController.updatePost);
 
-router.get("", checkAuth, (req, res, next) => {
-    Post.find().then(documents => {
-        res.status(200).json({
-            message: "Posts fetched successfully!",
-            posts: documents
-        });
-    });
-});
+router.get("", checkAuth, postController.getPost);
 
-router.get("/:id", checkAuth, (req, res, next) => {
-    Post.findById(req.params.id).then(post => {
-        if (post) {
-            res.status(200).json(post);
-        } else {
-            res.status(404).json({ message: "Post not found." });
-        }
-    });
-})
+router.get("/:id", checkAuth, postController.getPostById);
 
-router.delete("/:id", checkAuth, (req, res, next) => {
-    Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {
-        if (result.nModified > 0) {
-            res.status(200).json({ message: "Delete request successful!" });
-        } else {
-            res.status(401).json({ message: "Unauthorized DELETE request." });
-        }
-    });
-});
+router.delete("/:id", checkAuth, postController.deletePost);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,65 +1,11 @@
 const express = require("express");
-const User = require("../models/user");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+
+const UserController = require("../controllers/users")
+
 const router = express.Router();
 
-router.post("/register", (req, res, next) => {
-    bcrypt.hash(req.body.password, 11).then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save().then(result => {
-            res.status(201).json({
-                message: "User created!",
-                result: result
-            });
-        }).catch(err => {
-            res.status(500).json({
-                error: err
-            });
-        });
-    });
-});
+router.post("/register", UserController.createUser);
 
-router.post("/login", (req, res, next) => {
-    let fetchedUser;
-    User.findOne({ email: req.body.email })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({
-            message: "Authentication failed: User not found."
-          });
-        }
-        fetchedUser = user;
-        return bcrypt.compare(req.body.password, user.password);
-      })
-      .then(result => {
-        if (!result) {
-          return res.status(401).json({
-            message: "Authentication failed."
-          });
-        }
-        const token = jwt.sign(
-          { email: fetchedUser.email, userId: fetchedUser._id },
-          "secret_string_for_development",
-          { expiresIn: "1h" }
-        );
-        res.status(200).json({
-          token: token,
-          expiresIn: 3600,
-          userId: fetchedUser._id
-        });
-      })
-      .catch(err => {
-        return res.status(401).json({
-          message: "Authentication failed."
-        });
-      });
-  });
+router.post("/login", UserController.userLogin);
   
-  
-
 module.exports = router;
